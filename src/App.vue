1a1ca8cb7aa272a547a6d9c40c6e82edf3747a18
graphieros <alec.lloyd.probert@gmail.com>
2025-03-27 09:47:25 +0100

    Doc page

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,185 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref, computed } from "vue";
+import { BrandGithubFilledIcon, TimelineIcon } from "vue-tabler-icons";
+import { VueHiCode } from "vue-hi-code";
+import "tiny-spark/dist/tiny-spark.umd";
+import "vue-hi-code/style.css"
+
+const codeConfig = ref({
+  backgroundColor: 'rgb(210,210,210)',
+  baseTextColor: '#1A1A1A',
+  copyIconColor: '#1A1A1A',
+})
+
+function makeDs(n) {
+  let arr = [];
+  for(let i = 0; i < n; i += 1) {
+    arr.push(Math.round(Math.random()*100));
+  }
+  return `[${arr.toString()}]`;
+}
+
+const dataset = ref(makeDs(12));
+const dates = ref('["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"]')
+
+const installContent = ref(`npm i tiny-spark`);
+const setupContent = ref(`import "tiny-spark/dist/tiny-spark.umd"`);
+const codeContent = ref(`
+<div class="p-6 bg-app-bg-grey w-full max-w-[600px] mx-auto">
+  <div class="w-full h-[200px] mx-auto">
+    <div 
+      class="tiny-spark" 
+      data-curve="true"
+      data-animation="true"
+      data-line-color="#4A4A4A"
+      data-area-color="#1A1A1A10"
+      data-line-thickness="3"
+      data-responsive
+      data-plot-color="#2A2A2A"
+      data-plot-radius="3"
+      data-number-locale="en-US"
+      data-number-rounding="2"
+      data-indicator-color="#8A8A8A"
+      data-indicator-width="1"
+      data-set="${dataset.value}"
+      data-dates='${dates.value}'
+    />
+  </div>
+</div>`
+);
+
+const cssContent = ref(`
+/** the chart container (div element) */
+.tiny-spark {
+}
+
+/** the tooltip (div element), just display:none if you don't need it */
+.tiny-spark-tooltip {
+  background: rgba(240,240,240, 0.9);
+  border-radius: 2px;
+  box-shadow: 0 6px 3px -3px rgba(0,0,0,0.2);
+  padding: 0 8px;
+}
+
+/** the indicator (svg line element) */
+.tiny-spark-indicator {
+  /** for example: customize stroke-dasharray */
+}
+
+/** the plots (svg circle element) */
+.tiny-spark-datapoint-circle {
+  stroke: rgb(210,210,210);
+}
+
+/** the chart line (svg path element) */
+.tiny-spark-line-path {
+}
+
+/** the chart area (svg path element) */
+.tiny-spark-line-area {
+}
+`)
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <main class="w-full mx-auto max-w-[1200px] px-6">
+    <header class="flex flex-row justify-between place-items-center pt-12 pb-2">
+      <div class="text-4xl flex flex-row gap-2 place-items-end">
+        <div class="flex flex-row place-items-center gap-2">
+            <TimelineIcon class="text-red-100"/>
+            tiny-spark 
+        </div>
+        <small class="text-sm text-gray-700">v0.1.0</small>
+      </div>
+      <a class="p-1 bg-red-100 rounded-full hover:shadow-md transition-all" href="https://github.com/graphieros/tiny-spark" target="_blank">
+        <BrandGithubFilledIcon/>
+      </a>
+    </header>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <h1 class="pl-8 mb-12 max-w-[32ch] text-gray-700">An elegant, reactive and responsive sparkline chart solution without dependency.</h1>
+
+    <div class="w-full max-w-[220px] mx-auto mb-12">
+      <VueHiCode
+        :content="installContent"
+        v-bind="codeConfig"
+        language="javascript"
+      />
     </div>
-  </header>
+    <div class="w-full max-w-[600px] mx-auto mb-12">
+      <VueHiCode
+        :content="setupContent"
+        v-bind="{
+          ...codeConfig,
+          backgroundColor: '#2A2A2A',
+          copyIconColor: '#8A8A8A'
+        }"
+        language="javascript"
+      />
+    </div>
 
-  <main>
-    <TheWelcome />
+    <section>
+      <div class="p-6 bg-app-bg-grey w-full max-w-[600px] mx-auto">
+        <div class="w-full h-[200px] mx-auto">
+          <div 
+            class="tiny-spark" 
+            data-curve="true"
+            data-animation="true"
+            data-line-color="#4A4A4A"
+            data-area-color="#1A1A1A10"
+            data-line-thickness="3"
+            data-responsive
+            data-plot-color="#2A2A2A"
+            data-plot-radius="3"
+            data-number-locale="en-US"
+            data-number-rounding="2"
+            data-indicator-color="#8A8A8A"
+            data-indicator-width="1"
+            :data-set="dataset"
+            :data-dates="dates"
+          />
+        </div>
+      </div>
+      <div class="w-full mx-auto max-w-[600px] flex flex-col place-items-center justify-center mt-6">
+        The chart is reactive. Dynamic change in data attributes will update the chart.<br>Try it out:
+        <button class="bg-app-bg-grey py-1 px-4 rounded hover:shadow" @click="dataset = makeDs(12)">Random data</button>
+      </div>
+    </section>
+
+    <div class="w-full mx-auto my-12">
+      <VueHiCode
+        :content="codeContent"
+        v-bind="{
+          backgroundColor: '#2A2A2A',
+          copyIconColor: '#8A8A8A'
+        }"
+        language="html"
+      />
+    </div>
+
+    <div class="w-full mx-auto my-12">
+      <h2 class="mb-6 text-xl">tiny-spark is <strong>headless</strong>. Target the following css classes to customize your chart. Here is an example:</h2>
+      <VueHiCode
+        :content="cssContent"
+        v-bind="{
+          backgroundColor: '#2A2A2A',
+          copyIconColor: '#8A8A8A'
+        }"
+        language="css"
+      />
+    </div>
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<style>
+.tiny-spark-tooltip {
+  background: rgba(240,240,240, 0.9);
+  border-radius: 2px;
+  box-shadow: 0 6px 3px -3px rgba(0,0,0,0.2);
+  padding: 0 8px;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.tiny-spark-datapoint-circle {
+  stroke: rgb(210,210,210);
 }
 </style>
---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        "app-bg-grey": "rgb(210,210,210)",
+        "black": "#1a1a1a",
+        "black-100" : "#242424",
+        "app-green": "#42d392",
+        "app-green-dark": "#1d915d",
+        "app-green-light": "#87e6bb",
+        "app-green-light-extra": "#d5eee3",
+        "app-blue": "#5f8aee",
+        "app-blue-mid": "#83a4f2",
+        "app-blue-light": "#abc2f6",
+        "app-blue-dark": "#314063",
+        "app-orange": "#ff6600",
+        "app-orange-light": "#faa770",
+        "app-red": "#ff3700",
+        "app-yellow": "#c8c832",
+        "app-gold" : "#fdd663"
+      }
+    },
+  },
+  plugins: [],
+}
